55dbf98234b961a4a168641d116d73fbc0033ece
손량 <loop.infinitely@gmail.com>
2019-03-16 16:53:43 +0900

    solved an exercise

---
+++ day2/exercise.py
@@ -0,0 +1,15 @@
+x = 4
+y = 3
+S = x + y
+A = x * y
+B = x ** y
+C = y / x
+D = x ** y ** x
+print(S)
+print(A)
+print(B)
+print(C)
+print(A + B)
+print(x + y)
+print('D')
+print('x + y = %d' % (S))
